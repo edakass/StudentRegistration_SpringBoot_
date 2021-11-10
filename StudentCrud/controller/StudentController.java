1599d359e42e3d5eb04f2ec36478a354cf62f8d7
Eda Kaş <61595808+edakass@users.noreply.github.com>
2021-11-10 18:25:11 +0300

    Add files via upload

--- StudentCrud/controller/StudentController.java
+++ StudentCrud/controller/StudentController.java
@@ -1,14 +1,19 @@
 package com.example.StudentCrud.controller;
 
 import com.example.StudentCrud.domain.Student;
+import com.example.StudentCrud.exception.StudentAlreadyExistsException;
 import com.example.StudentCrud.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 @Controller
 public class StudentController {
@@ -43,9 +48,20 @@ public class StudentController {
         return mav;
 
     }
+
     @RequestMapping("/delete/{id}")
-    public String deletestudent(@PathVariable(name = "id") int id) {
+    public String deletestudent(@PathVariable(name = "id") int id, RedirectAttributes attributes) {
         service.delete(id);
+        attributes.addFlashAttribute("success","The student has deleted !");
         return "redirect:/";
     }
+
+    /*
+    @ExceptionHandler(StudentAlreadyExistsException.class)
+    public ResponseEntity<String> handleCityAlreadyExistsException(StudentAlreadyExistsException ex){
+        return new ResponseEntity<>(ex.getMessage(), CONFLICT);
+    }
+
+     */
+
 }
--- StudentCrud/domain/Student.java
+++ StudentCrud/domain/Student.java
@@ -1,17 +1,24 @@
 package com.example.StudentCrud.domain;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 public class Student {
+
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+
     private String studentname;
+
     private String course;
+
     private int fee;
     public Student() {
 
